b46ac15cce2a364f08e76d061ca19c89aef052ec
MortezaRg <cloner6420@gmail.com>
2023-06-01 15:48:36 +0330

    initial commit and Add Firebase to project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chatapp/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    // for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    // for android and ios
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:701855335130:web:26b061d937116e1e775be3";
+  static String apiKey = "AIzaSyAn46X6wu-WPr9u-MmAw6kJ0qbIRjDzK2U";
+  static String messagingSenderId = "701855335130";
+  static String projectId = "chatapp-432df";
+}
